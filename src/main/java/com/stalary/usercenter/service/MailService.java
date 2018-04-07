dcc828c9b80fdd70d648809e1beeb2d0eb18c99f
stalary <452024236@qq.com>
2018-04-07 13:25:43 +0800

    MOD: 修复邮箱问题，需要先抄送给自己

--- src/main/java/com/stalary/usercenter/service/MailService.java
+++ src/main/java/com/stalary/usercenter/service/MailService.java
@@ -34,14 +34,15 @@ public class MailService {
     public void sendSimpleMail(String receiver) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(sender);
-        message.setTo(receiver);
+        message.setTo(sender);
         message.setSubject("异地登录警告");
         message.setText("检测到您的账号存在异地登录，请确定是否为您本人操作，如果非本人操作，则账号存在被盗风险！");
+        message.setCc(receiver);
         try {
             mailSender.send(message);
             log.info("user_log" + UCUtil.SPLIT + UCUtil.MAIL + UCUtil.SPLIT + -1 + UCUtil.SPLIT + "异地警报邮件发送成功");
         } catch (MailException e) {
-            log.warn("user_log" + UCUtil.SPLIT + UCUtil.MAIL + UCUtil.SPLIT + -1 + UCUtil.SPLIT + "异地警报邮件发送失败");
+            log.warn("user_log" + UCUtil.SPLIT + UCUtil.MAIL + UCUtil.SPLIT + -1 + UCUtil.SPLIT + "异地警报邮件发送失败" + e);
         }
     }
 }
